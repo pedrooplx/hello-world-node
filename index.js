1c445c36931777935b6dc3bce0a8bea0bf6745b1
Pedro Henrique <pedrooplx@gmail.com>
2022-09-12 20:54:03 -0300

    create pck.json

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
-};
+var express = require('express')
+var app = express();
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
