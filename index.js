de1e5dd5b61fcce7bf5431e536ecca34f3114f04
Pedro Henrique <56414441+pedrooplx@users.noreply.github.com>
2022-09-14 21:18:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Olá mundo!' + new Date().toISOString())
+  response.send('Olá mundo! Horário ' + new Date().toISOString())
 })
 
 app.listen(app.get('port'), function() {
